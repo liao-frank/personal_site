5d260afd50e658676fc95a10b57c928652469931
Frank Liao <recs7168@gmail.com>
2017-09-10 14:17:10 -0400

    Video functionality!

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,7 +12,6 @@ class Project < ApplicationRecord
 
 	# Validations
 	validates :title, presence: true
-	validates_numericality_of :order, only_integer: true, greater_than_or_equal_to: 0
 	# TODO uniqueness of :order per project id validation
 	# TODO cover url regex validation
 
